fbcb3230fe4d0d2d10e9c3ae96bbca46bae9c6f3
fsteinha <franksteinhans@yahoo.com>
2023-02-21 21:13:59 +0100

    bugfix cube example

--- examples/exam_cube.py
+++ examples/exam_cube.py
@@ -4,6 +4,6 @@ sys.path.append("../src")
 import pcad as pcad
 import const_scad as const
 
-cube = pcad.cube(pcad.pos(10, 10, 10))
+cube = pcad.cube(10, 10, 10)
 scad = const.scad("test", cube)
 scad.show()
