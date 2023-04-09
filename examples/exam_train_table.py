fda0719f5e62ee628c66da7b247896f0153c8225
fsteinha <franksteinhans@yahoo.com>
2023-04-09 08:37:15 +0200

    right left crossbars in train table example

--- examples/exam_train_table.py
+++ examples/exam_train_table.py
@@ -12,8 +12,9 @@ import exam_parse
 ##############################################################################
 
 TABLE_HEIGTH = 600
-TABLE_INTER_HEIGH = 300
+TABLE_INTER_HEIGH = 100
 TABLE_DEPTH = 850
+TABLE_DEPTH_OFFSET = 1000
 TABLE_WITH  = 2000
 TABLE_SPACE_FROM_RIGTH = 600
 
@@ -77,16 +78,26 @@ pole_t5.add(pcad.cube(BARY_X, POLE_Y, BARX_Z, pos=pcad.pos(0,0,TABLE_HEIGTH-BARX
 pole_t5.add(pcad.cube(POLE_X, BARX_Y, BARX_Z, pos=pcad.pos(0,POLE_Y-BARX_Y,TABLE_INTER_HEIGH-BARX_Z)))
 pole_t5.add(pcad.cube(BARY_X, POLE_Y, BARX_Z, pos=pcad.pos(0,0,TABLE_INTER_HEIGH-BARX_Z)))
 
-crossbar_x = pcad.cube(TABLE_WITH,
+crossbar_x_front = pcad.cube(TABLE_WITH,
                        Unterkonstruktionslatte_2000_40_18.dy,
                        Unterkonstruktionslatte_2000_40_18.dx)
-crossbar_x.set_color(pcad.RGBColor.DARK_RED)
+crossbar_x_front.set_color(pcad.RGBColor.DARK_RED)
+
+crossbar_x_behind = pcad.cube(TABLE_WITH - 2 * Unterkonstruktionslatte_2000_40_18.dy,
+                       Unterkonstruktionslatte_2000_40_18.dy,
+                       Unterkonstruktionslatte_2000_40_18.dx)
+crossbar_x_behind.set_color(pcad.RGBColor.DARK_RED)
+
 
 crossbar_y = pcad.cube(Unterkonstruktionslatte_2000_40_18.dy,
                        TABLE_DEPTH - 2 *Unterkonstruktionslatte_2000_40_18.dy,
                        Unterkonstruktionslatte_2000_40_18.dx)
 crossbar_y.set_color(pcad.RGBColor.RED)
 
+crossbar_y_top = pcad.cube(Unterkonstruktionslatte_2000_40_18.dy,
+                       TABLE_DEPTH_OFFSET - Unterkonstruktionslatte_2000_40_18.dy,
+                       Unterkonstruktionslatte_2000_40_18.dx)
+crossbar_y_top.set_color(pcad.RGBColor.RED)
 
 # construction
 ##############################################################################
@@ -100,7 +111,7 @@ pole_left_front  = copy.copy(pole_t1)
 pole_left_front.set_name(None)
 
 pole_right_behind = copy.copy(pole_t3)
-pole_right_behind.pos = pcad.pos(TABLE_WITH,TABLE_DEPTH,0)
+pole_right_behind.pos = pcad.pos(TABLE_WITH-POLE_X,TABLE_DEPTH,0)
 pole_right_behind.rot = pcad.rot(0,0,-90)
 pole_right_behind.set_name(None)
 
@@ -119,29 +130,37 @@ pole_middle_behind.pos = pcad.pos(TABLE_WITH-TABLE_SPACE_FROM_RIGTH-POLE_X,TABLE
 pole_middle_behind.rot = pcad.rot(0,0,-90)
 pole_middle_behind.set_name(None)
 
-cross_bar_front_top = copy.copy(crossbar_x)
+cross_bar_front_top = copy.copy(crossbar_x_front)
 cross_bar_front_top.pos = pcad.pos(0,0,TABLE_HEIGTH-BARX_Z)
 cross_bar_front_top.set_name(None)
 
-cross_bar_behind_top = copy.copy(crossbar_x)
-cross_bar_behind_top.pos = pcad.pos(0,TABLE_DEPTH-BARX_Y,TABLE_HEIGTH-BARX_Z)
+cross_bar_behind_top = copy.copy(crossbar_x_behind)
+cross_bar_behind_top.pos = pcad.pos(Unterkonstruktionslatte_2000_40_18.dy,TABLE_DEPTH-BARX_Y,TABLE_HEIGTH-BARX_Z)
 cross_bar_behind_top.set_name(None)
 
-cross_bar_front_mid = copy.copy(crossbar_x)
+cross_bar_front_mid = copy.copy(crossbar_x_front)
 cross_bar_front_mid.pos = pcad.pos(0,0,TABLE_INTER_HEIGH-BARX_Z)
 cross_bar_front_mid.set_name(None)
 
-cross_bar_behind_mid = copy.copy(crossbar_x)
+cross_bar_behind_mid = copy.copy(crossbar_x_front)
 cross_bar_behind_mid.pos = pcad.pos(0,TABLE_DEPTH-BARX_Y,TABLE_INTER_HEIGH-BARX_Z)
 cross_bar_behind_mid.set_name(None)
 
-cross_bar_left = copy.copy(crossbar_y)
-cross_bar_left.pos = pcad.pos(0,crossbar_x.dy,TABLE_INTER_HEIGH-BARX_Z)
+cross_bar_left = copy.copy(crossbar_y_top)
+cross_bar_left.pos = pcad.pos(0,cross_bar_left.dx,TABLE_HEIGTH-BARX_Z)
 cross_bar_left.set_name(None)
 
-cross_bar_right = copy.copy(crossbar_y)
-cross_bar_right.pos = pcad.pos(0,crossbar_x.dy,TABLE_INTER_HEIGH-BARX_Z)
+cross_bar_right = copy.copy(crossbar_y_top)
+cross_bar_right.pos = pcad.pos(TABLE_WITH-cross_bar_right.dx,cross_bar_right.dx,TABLE_HEIGTH-BARX_Z)
 cross_bar_right.set_name(None)
+
+cross_bar_left_mid = copy.copy(crossbar_y)
+cross_bar_left_mid.pos = pcad.pos(0,cross_bar_left_mid.dx,TABLE_INTER_HEIGH-BARX_Z)
+cross_bar_left_mid.set_name(None)
+
+cross_bar_right_mid = copy.copy(crossbar_y)
+cross_bar_right_mid.pos = pcad.pos(TABLE_WITH-cross_bar_right_mid.dx,cross_bar_right_mid.dx,TABLE_INTER_HEIGH-BARX_Z)
+cross_bar_right_mid.set_name(None)
 
 # show
 ##############################################################################
@@ -159,7 +178,9 @@ if exam_parse.M_SCAD==True:
                                  cross_bar_front_mid,
                                  cross_bar_behind_mid,
                                  cross_bar_left,
-                                 cross_bar_right)
+                                 cross_bar_right,
+                                 cross_bar_left_mid,
+                                 cross_bar_right_mid)
     scad.show()
 else:
     constcq = const_cadquery.cq_const(exam_parse.get_const_name(__file__), pole_t1)
