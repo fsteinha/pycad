21b2ea88d9a28e437a35332f539d437c56fdefd8
fsteinha <franksteinhans@yahoo.com>
2023-08-31 15:47:10 +0200

    Rework program interface for scad

--- prog/prog_parse.py
+++ prog/prog_parse.py
@@ -3,6 +3,15 @@ import re
 import os
 import sys
 
+D_PROG_OPTIONS = {
+    "SCAD": {
+        "Enable":True,
+        "PATH": "openscad",
+    },
+    "CQ": False,
+}
+
+
 # Functions
 ##############################################################################
 def get_const_name(filename:str):
@@ -29,33 +38,31 @@ def exam_execute(objs:list, purch = False, file_name:str=sys.argv[0]):
     """
     if file_name == None:
         file_name = get_const_name(__file__)
-    if M_SCAD==True:
+    if D_PROG_OPTIONS["SCAD"]["Enable"] ==True:
         sys.path.append("../")
         import const.scad.const_scad as const_scad
         scad = const_scad.scad_const(file_name, objs)
-        scad.show()
-    elif M_CQ==True:
+        scad.show(D_PROG_OPTIONS["SCAD"]["PATH"])
+    elif D_PROG_OPTIONS["CQ"]["Enable"]==True:
         import const_cadquery
         constcq = const_cadquery.cq_const(file_name, objs)
-        constcq.show()
+        constcq.show(D_PROG_OPTIONS["CQ"]["PATH"])
     else:
         raise Exception ("Unknown option")
 
 # parse arguments
 ##############################################################################
 parser = argparse.ArgumentParser(description='Process some integers.')
-parser.add_argument('--m', choices=['scad', 'cq'], default='cq',
-                    help='scad or cadquery')
+# scad parameter
+parser.add_argument("--scad", nargs='?', const="openscad", help="activate scad with option program path")
 
 args = parser.parse_args()
 
-M_SCAD = False
-M_CQ = False
-
-match args.m:
-    case 'scad':
-        M_SCAD=True
-    case 'cq':
-        M_CQ =True
-    case other:
-        M_SCAD =True
+# Die Werte der Argumente abrufen und verwenden
+if args.scad:
+    D_PROG_OPTIONS["SCAD"]["Enable"] = True
+    if args.scad is True:
+        D_PROG_OPTIONS["SCAD"]["PATH"] = "openscad"
+    else:
+        D_PROG_OPTIONS["SCAD"]["PATH"] = args.scad.replace("/","\\")
+        
